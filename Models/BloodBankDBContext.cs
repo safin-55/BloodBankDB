ef81e110928473f3ff28b4565dab09d7d0b082d1
safin-55 <mohammadsafin34@gmail.com>
2026-07-24 20:02:54 +0600

    Initial commit: ASP.NET Core MVC project with scaffolded models

---
+++ Models/BloodBankDBContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodBankApp.Models;
+
+public partial class BloodBankDBContext : DbContext
+{
+    public BloodBankDBContext()
+    {
+    }
+
+    public BloodBankDBContext(DbContextOptions<BloodBankDBContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Donation> Donations { get; set; }
+
+    public virtual DbSet<Donor> Donors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost,1433;Database=BloodBankDB;User Id=sa;Password=Safin55555##;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Donation>(entity =>
+        {
+            entity.HasKey(e => e.DonationId).HasName("PK__Donation__C5082EFB2A4F89C1");
+
+            entity.ToTable("Donation");
+
+            entity.Property(e => e.CampName).HasMaxLength(100);
+
+            entity.HasOne(d => d.Donor).WithMany(p => p.Donations)
+                .HasForeignKey(d => d.DonorId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Donation_Donor");
+        });
+
+        modelBuilder.Entity<Donor>(entity =>
+        {
+            entity.HasKey(e => e.DonorId).HasName("PK__Donor__052E3F78F8F97843");
+
+            entity.ToTable("Donor");
+
+            entity.Property(e => e.BloodGroup).HasMaxLength(5);
+            entity.Property(e => e.City).HasMaxLength(50);
+            entity.Property(e => e.ContactNo).HasMaxLength(20);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BloodBankApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
